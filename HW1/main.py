05ec8ca192e04b2faf153522664a7fb98f4b23d6
Jenny Haselden <jennyhaselden7@gmail.com>
2024-09-05 16:41:41 -0700

    tuple changes work!

--- HW1/main.py
+++ HW1/main.py
@@ -14,6 +14,11 @@ def tuple_arg(input):
         return False
 
 
+def delete_element(tuple, index):
+    new_tuple = tuple[:index] + tuple[index + 1:]
+    return new_tuple
+
+
 def find_palindrome(pattern):
 
     if not tuple_arg(pattern):
@@ -23,71 +28,30 @@ def find_palindrome(pattern):
 
     if length <= 2:                 # Returned palindrome must be at least 2 elements 
         return None
-    
-    pattern_list = list(pattern)    # List conversion allows removal of an element
 
     left = 0                       
     right = length - 1
     removed = 0
+    delete = None
 
     while left <= right and removed < 1:
-        # if pattern_list[left] == ' ':
-        #     left += 1
-
-        # if pattern_list[right] == ' ':
-        #     right -= 1
-
-        if pattern_list[left] == pattern_list[right]:   # Match? Move inward to next comparison    
+        if pattern[left] == pattern[right]:   # Match? Move inward to next comparison    
             left += 1
             right -= 1
         else:
             if left + 1 == right:       # Remove middle to make palindrome, pattern is even
-                del pattern_list[left]
-            elif pattern_list[left + 1] == pattern_list[right]:
-                del pattern_list[left]
-            elif pattern_list[left] == pattern_list[right - 1]:
-                del pattern_list[right]
+                delete = left
+            elif pattern[left + 1] == pattern[right]:
+                delete = left
+            elif pattern[left] == pattern[right - 1]:
+                delete = right
             else:                       # There is no possible palindrome with one removal
-                return None
-
+                return None         # this isn't returning none!? maybe??
+            
             removed += 1
-            left += 1
-            right -= 1
 
-    if removed == 0:                    # pattern was already a palindrome. remove middle
-        del pattern_list[math.floor(length / 2)]
+    if removed == 0:                    # Pattern was already a palindrome, remove middle
+        delete = math.floor(length / 2)
 
-    pattern = tuple(pattern_list)
+    pattern = delete_element(pattern, delete)
     return pattern
-
-
-# print('example 1:', find_palindrome((1, 2)), 'expected: None') 
-# print('example 2:', find_palindrome((1, 2, 3, 2, 1)), 'expected: ( 1 2 2 1 )')
-# print('example 3:', find_palindrome((1, 2, 3, 4)), 'expected: None' )
-# print('example 4:', find_palindrome((1, 2, 3, 2, 4, 1)), 'expected: ( 1 2 3 2 1 )')
-# print('example 5:', find_palindrome((1, 2, 3, 4, 2, 1)), 'expected: ( 1 2 4 2 1 )')
-# print('example 6:', find_palindrome(('c', 'i', 'v', 'i', 'c')), 'expected: ( c i i c )')
-# print('example 7:', find_palindrome(('a', 1, 2, 2, 1, 'a')), 'expected: ( a 1 2 1 a )')
-
-# input8 = tuple("Ma is as selfless as I am")
-# print(input8)
-# print('example 8:', find_palindrome(input8), 'expected: ( ? )')
-
-# input8b = tuple("ma is as selfless as i am")
-# print(input8b)
-# print('example 8b:', find_palindrome(input8b), 'expected: ( m a  i s  a s  s e l f l e s s  a s  i  a m )')
-
-
-# print('example 9:', find_palindrome("string"), 'expected: None')
-# print('example 10:', find_palindrome(["this", "is", "not", "a", "tuple"]), 'expected: None')
-# print('example 11: ', find_palindrome(3), 'expected: None')
-
-# input12 = tuple("Able was I ere I saw Elba")
-# print(input12)
-# print('example 12:', find_palindrome(input12), 'expected: ( ? )')
-
-# input13 = ("Able", "was", "I", "ere", "I", "saw", "Elba")
-# print('example 13:', find_palindrome(input13), 'expected: None')
-
-# input14 = tuple("ever")
-# print('example 14:', find_palindrome(input14), 'expected: ( e v e )')
